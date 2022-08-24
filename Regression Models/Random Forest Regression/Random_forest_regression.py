e36126e0d0740fc7011720ab6051025e0edc4594
Ashwinshibu <111752156+Ashwinshibu@users.noreply.github.com>
2022-08-24 11:53:33 +0530

    Update Random_forest_regression.py

--- Regression Models/Random Forest Regression/Random_forest_regression.py
+++ Regression Models/Random Forest Regression/Random_forest_regression.py
@@ -3,7 +3,7 @@
 
 # In[2]:
 
-
+#import required libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 # In[3]:
 
-
+#load data
 data=pd.read_csv(r"C:\Users\ABIN\Desktop\PYSPARK MLLIB\Height_age_data.csv")
 
 
@@ -23,7 +23,7 @@ data
 
 # In[15]:
 
-
+#seperate indpendent and dependent variable values
 x=data.iloc[:,0:1].values
 
 
@@ -46,7 +46,7 @@ y.shape
 
 
 # In[17]:
-
+#split train and test data from dependent and independent values
 
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=0)
@@ -54,25 +54,25 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=0)
 
 # In[18]:
 
-
+#import RandomForestRegressor 
 from sklearn.ensemble import RandomForestRegressor
 
 
 # In[37]:
 
-
+#create object with required  no of estimators
 RF=RandomForestRegressor(n_estimators=500,random_state=0)
 
 
 # In[38]:
 
-
+#Fit the random forest regressor with training data represented by X_train and y_train
 RF.fit(x_train,y_train)
 
 
 # In[39]:
 
-
+#predict dependent value
 y_predict=RF.predict(x_test)
 
 
@@ -84,7 +84,7 @@ y_predict
 
 # In[41]:
 
-
+#import metrics function to find r2 value
 from sklearn import metrics
 r2=metrics.r2_score(y_test,y_predict)
 
@@ -96,7 +96,7 @@ r2
 
 
 # In[43]:
-
+#visualize actual and predicted data 
 
 plt.scatter(x_train,y_train,color="green")
 x_val=np.arange(min(x_train),max(x_train),0.01)
@@ -129,7 +129,7 @@ plt.show()
 
 # In[47]:
 
-
+#Predicting dependent data(height) from input(age)
 height_pred=RF.predict([[41]])
 
 
